5e82a04a3b1685d21a449a73ad29c7ac828760c3
Kubisz Mariusz <kontakt@kubiszmariusz.pl>
2022-10-09 12:56:34 +0200

    Support for building the latest version

--- hasura-cli/metadata.sh
+++ hasura-cli/metadata.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-echo "::set-output name=image_name::hasura-cli:$Image_Version"
+#!/bin/bash
+if [ -z $Image_Version ]; then
+  echo "::set-output name=image_name::hasura-cli:latest"
+else
+  echo "::set-output name=image_name::hasura-cli:$Image_Version"
+fi
--- hasura-wait4x/metadata.sh
+++ hasura-wait4x/metadata.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-echo "::set-output name=image_name::hasura-wait4x:$Image_Version"
+#!/bin/bash
+if [ -z $Image_Version ]; then
+  echo "::set-output name=image_name::hasura-wait4x:latest"
+else
+  echo "::set-output name=image_name::hasura-wait4x:$Image_Version"
+fi
